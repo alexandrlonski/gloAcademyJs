e19363181edc6e09ac156ff1a2d21bec490aafb7
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-09 13:40:09 +0300

    Выполнили задание №2

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  peroid;
+const money = 3000,
+  income = "фриланс",
+  addExpenses = "Питание, Платежи, Здоровье",
+  deposit = false,
+  mission = 20000,
+  period = 12,
+  budgetDay = 3000 / 30;
 
-alert('hello');
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+console.log(addExpenses.toLowerCase().split(", "));
 
-console.log('we are learning js');
+console.log(budgetDay);
