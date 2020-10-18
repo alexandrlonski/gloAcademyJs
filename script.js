eab9fb1763d2c4a36e09b5cbc527564782544ee2
Alexandr <aleksandr.lonski.86@gmail.com>
2020-10-18 19:14:57 +0300

    Спрятал console.log

--- script.js
+++ script.js
@@ -27,24 +27,29 @@ let appData = {
     for(let key in appData.expenses){
         appData.expensesMonth += appData.expenses[key];
       }
-      
+    console.log('Расходы за месяц: ' + appData.expensesMonth);  
      },
   getBudget: function() {
      appData.budgetMonth = money - appData.expensesMonth ;
      appData.budgetDay = Math.floor(appData.budgetMonth / 30);
   },  
   getTargetMonth: function() {
-     return Math.ceil(appData.mission / appData.budgetMonth);
+     appData.getTargetMonth = Math.ceil(appData.mission / appData.budgetMonth);
+     if (appData.getTargetMonth <= 0) {
+        console.log('цель небудет достигнута, вы слишком много тратите');
+      } else {
+        console.log('Cрок достижения цели : ' + appData.getTargetMonth);
+      } 
   }, 
   getStatusIncome: function() {
       if (appData.budgetDay > 1200){
-       return ('У вас высокий уровень дохода');
+      console.log('У вас высокий уровень дохода');
     } else if (appData.budgetDay <= 1200 && appData.budgetDay > 600){
-      return ('У вас средний уровень дохода');
+      console.log('У вас средний уровень дохода');
     } else if (appData.budgetDay <= 600 && appData.budgetDay > 0){
-      return ('К сожалению у вас уровень дохода ниже среднего');
+      console.log('К сожалению у вас уровень дохода ниже среднего');
     } else {
-      return ('Вы слишком много тратите');
+      console.log('Вы слишком много тратите');
     }
   },
   asking: function() {
@@ -71,16 +76,6 @@ appData.getBudget();
 appData.getTargetMonth();
 appData.getStatusIncome();
 
-console.log('Расходы за месяц: ' + appData.expensesMonth);
-
-if (appData.getTargetMonth() <= 0) {
-  console.log('цель небудет достигнута, вы слишком много тратите');
-} else {
-console.log('Cрок достижения цели : ' + appData.getTargetMonth()) ;
-} 
-
-console.log(appData.getStatusIncome());
-  
 for(let key in appData) {
   console.log(`Наша программа включает в себя данные: ${key} = ${appData[key]}`);
 }
